231d02b9a8b2d48dbb63f33cb05fbdc69d08c4a1
Christine Lao <clao@ucsd.edu>
2016-06-27 00:51:35 -0700

    Fix menu bugs

--- js/project.js
+++ js/project.js
@@ -2,46 +2,17 @@ window.onload = responsive;
 window.onresize = responsive;
 
 function responsive(){
-	var mq = window.matchMedia( "(max-width: 1550px)" );
-  var mq775 = window.matchMedia( "(max-width: 775px)" );
-  var mq450 = window.matchMedia( "(max-width: 450px)" );
+  var mq880 = window.matchMedia( "(max-width: 880px)" );
   var matched;
 
-  if (mq.matches) {
-    /* Remove 'Contact Me'
-    document.querySelector('.contact h2').innerHTML = '';
-  } 
-  else{
-    document.querySelector('.contact h2').innerHTML = 'Contact Me';*/
-  }
-
-  if (mq775.matches){
+  if (mq880.matches){
     matched = true; 
-    // Remove Name 
-    //document.querySelectorAll('.footer > h2')[0].innerHTML = ''; 
-
-    // Hide navigation menus
-    //$('.navbar').css('display', 'none');
-
-    //$('#navbar--mobile').css('display', 'block');
-    //$('#navbar--mobile').css('position', 'fixed');
 
     $('button').on('click', function(e){
-      $('#mobilelinks').slideToggle('slow'); 
+      $('#navbar--mobile__links').slideToggle('slow'); 
     });
   }
-  else{
-    document.querySelectorAll('footer > h2')[0].innerHTML = 'Christine Lao'; 
-    $('.navbar').css('display', 'none');
-    $('#navbar--hidden').css('display', 'block');
-  }
-
-  if (mq450.matches){
-    $('.thirdproj .overlay').css('width','278px');
-    $('.thirdproj .overlay').css('height','276px');
-  }
-	
-  // Show onpage menu
+  
   if (!matched){
     $('#navbar--hidden').css('display', 'block');
   }
