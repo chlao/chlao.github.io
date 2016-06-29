717bb642ae31ae79536c6167f2b7f758928dc4b8
Christine Lao <clao@ucsd.edu>
2016-06-29 10:16:06 -0700

    Fixed Resume page

--- Resume/resume.js
+++ Resume/resume.js
@@ -2,19 +2,8 @@ window.onload = responsive;
 window.onresize = responsive;
 
 function responsive(){
-	var mq = window.matchMedia( "(max-width: 1550px)" );
-  var mq775 = window.matchMedia( "(max-width: 775px)" );
-  var mq450 = window.matchMedia( "(max-width: 450px)" );
   var mq1050 = window.matchMedia( "(max-width: 1050px)" );
-  var matched;
-
-  if (mq.matches) {
-    /* Remove 'Contact Me'*/
-    document.querySelector('.contact h2').innerHTML = '';
-  } 
-  else{
-    document.querySelector('.contact h2').innerHTML = 'Contact Me';
-  }
+  var mq880 = window.matchMedia( "(max-width: 880px)" );
 
   if (mq1050.matches){
     $(".contact > a").contents().filter(function () {
@@ -25,7 +14,6 @@ function responsive(){
     $('.heading > .contact img:first-child').css('margin', '5px'); 
   }
   else{
-
     $('.heading > .contact img').removeClass('icon'); 
     $('.heading > .contact img:first-child').css('margin', '15px'); 
 
@@ -35,32 +23,9 @@ function responsive(){
     }
   }
 
-  if (mq775.matches){
-    matched = true; 
-    // Remove Name 
-    document.querySelectorAll('.footer > h2')[0].innerHTML = ''; 
-
-    // Hide navigation menus
-    $('#hiddennav').css('display', 'none');
-
-    $('#mobilenav').css('display', 'block');
-    $('#mobilenav').css('position', 'fixed');
-
-    $('button').on('click', function(e){
-      $('#mobilelinks').slideToggle('slow'); 
+  if (mq880.matches){
+    $('#navbar--mobile button').on('click', function(e){
+      $('#navbar--mobile__links').slideToggle('slow'); 
     });
-
-    // Resume heading 
-
   }
-  else{
-    document.querySelectorAll('.footer > h2')[0].innerHTML = 'Christine Lao'; 
-    $('.navbar').css('display', 'none');
-    $('#hiddennav').css('display', 'block');
-  }
-
-  // Show onpage menu
-  if (!matched){
-    $('#hiddennav').css('display', 'block');
-  }
-}; 
+} 
